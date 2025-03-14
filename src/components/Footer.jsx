c0a283f5f7388c6e6b039e207251e11c58a14c60
kapppitan <jeikabryll2020@gmail.com>
2025-03-14 19:26:57 +0800

    Made a hero and some placeholder movies.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return (
+        <div className="h-fit bg-black p-20 flex" id="footer">
+            <div className="flex flex-col gap-2 text-center text-white">
+                <h1 className="text-5xl text-white">Contact Us</h1>
+                <p className="text-gray-600 text-3xl font-mono">example@gmail.com</p>
+            </div>
+        </div>
+    )
+}
